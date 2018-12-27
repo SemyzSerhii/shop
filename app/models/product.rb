7ff9fe59b2861064b4295193ad033a0023071b60
marina8915 <nonalatus@ukr.net>
2018-12-28 00:02:39 +0200

    added product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ },
+            numericality: { greater_than: 0, less_than: 1000000 }
+  validates :short_description, presence: true, length: { minimum: 10, maximum: 200 }
+  validates :full_description, presence: true, length: { minimum: 20, maximum: 3000 }
+
 end
