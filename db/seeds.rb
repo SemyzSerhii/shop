830f1e234241756b10b8a8b8753ef7e4ed65b2bf
marina8915 <nonalatus@ukr.net>
2018-12-29 21:35:12 +0200

    changed config

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'Admin', email: 'admin@gmail.com', password: '1111aQ', role: 'admin', access: true)
+User.create(name: 'Admin', email: 'admin@gmail.com', password: '1111aQ', access: true)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
