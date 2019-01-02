8e00f5e9db4b92be812d11248601a50aa3c7b528
marina8915 <nonalatus@ukr.net>
2019-01-02 20:05:20 +0200

    fixed user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.access = true
-    @user.role = 'user'
 
     respond_to do |format|
       if @user.save
