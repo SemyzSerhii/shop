3a66b536443a05fc833c64a48b12bf7a409b2d91
marina8915 <nonalatus@ukr.net>
2019-01-22 21:35:47 +0200

    count cart

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -7,10 +7,13 @@ module CurrentCart
       session[:cart_id] = current_user.carts.last.id if current_user.carts.last.present?
     end
     @cart = Cart.find(session[:cart_id])
+    @count = 0
+    @cart.line_items.each { |p| @count += p.quantity }
   rescue ActiveRecord::RecordNotFound
     if current_user
       @cart = current_user.carts.create
       session[:cart_id] = @cart.id
+      @count = 0
     else
       @cart = Cart.new
     end
