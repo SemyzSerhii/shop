803ba6c6763a5917818366bc1558fdec0424fb10
marina8915 <nonalatus@ukr.net>
2019-01-22 10:00:27 +0200

    fixed user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    if check_access(@user)
+    if current_user
       respond_to do |format|
         if @user.update(user_params)
           format.html { redirect_to @user, notice: 'User was successfully updated.' }
